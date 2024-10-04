582024dfa09614d5b044740e5b3b977eecc11d53
David M <david.mellert@gmx.de>
2024-10-04 20:50:53 +0200

    Add trace similarity using levenshtein distance

--- flask-server/lpm_set_comparison_python/similarity_computation.py
+++ flask-server/lpm_set_comparison_python/similarity_computation.py
@@ -1,8 +1,11 @@
 from .lpm import LPM, LPMSet
 from typing import List, Callable
 import numpy as np
+from scipy.optimize import linear_sum_assignment
+from difflib import SequenceMatcher
+from pm4py.algo.clustering.trace_attribute_driven.leven_dist.leven_dist_calc import leven_preprocess
 
-def compute_trace_similarity(lpm_a: LPM, lpm_b: LPM):
+def compute_trace_similarity_perfect(lpm_a: LPM, lpm_b: LPM):
     traces_a = lpm_a.get_traces()
     traces_b = lpm_b.get_traces()
 
@@ -10,6 +13,26 @@ def compute_trace_similarity(lpm_a: LPM, lpm_b: LPM):
     intersection_traces = traces_a.intersection(traces_b)
 
     return len(intersection_traces) / len(union_traces)
+
+def compute_trace_similarity_leven(lpm_a: LPM, lpm_b: LPM):
+    traces_a = lpm_a.get_traces()
+    traces_b = lpm_b.get_traces()
+
+    if len(traces_a) == 0 or len(traces_b) == 0:
+        return 0
+
+    similarity_matrix = np.zeros((len(traces_a), len(traces_b)))
+
+    for i, trace_a in enumerate(traces_a):
+        for j, trace_b in enumerate(traces_b):
+                str1, str2 = leven_preprocess(list(trace_a), list(trace_b))
+                normalized_inverted_levenshtein = SequenceMatcher(None, str1, str2).ratio()
+                similarity_matrix[i, j] = normalized_inverted_levenshtein
+    
+    row_ind, col_ind = linear_sum_assignment(similarity_matrix, maximize=True)
+    trace_gain = similarity_matrix[row_ind, col_ind].sum()
+
+    return 2 * trace_gain / (len(traces_a) + len(traces_b))
 
 def compute_eventually_follows_similarity(lpm_a: LPM, lpm_b: LPM):
     eventually_follows_a = lpm_a.get_eventually_follows_set()
@@ -35,6 +58,7 @@ def compute_pairwise_similarity_measures(set_a: LPMSet, set_b: LPMSet, similarit
     
     return similarity_matrix
 
-
 def compute_similarity_measures(set_a: LPMSet, set_b: LPMSet):
+    #print(compute_trace_similarity_leven(set_a.lpms[0], set_b.lpms[0]))
     print(np.array(compute_pairwise_similarity_measures(set_a, set_b, compute_eventually_follows_similarity)))
+    print(np.array(compute_pairwise_similarity_measures(set_a, set_b, compute_trace_similarity_leven)))
