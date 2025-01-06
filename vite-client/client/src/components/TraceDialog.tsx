ca09ba501a7ad141a25748a0d032cb1b9b89e09d
David <david.mellert@rwth-aachen.de>
2025-01-06 19:14:50 +0100

    Adding an event coverage tooltip

--- vite-client/client/src/components/TraceDialog.tsx
+++ vite-client/client/src/components/TraceDialog.tsx
@@ -2,6 +2,7 @@ import { Trace } from "@/types/Report";
 import { Dialog, DialogHeader, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 type TraceDetails = {
     event: string;
@@ -53,14 +54,32 @@ export default function TraceDialog({
           <div className="flex items-center justify-center px-0 py-8 min-w-fit">
             {traceDetails?.map((detail, index) => (
               <div key={index} className="flex items-center">
-                <div
-                  className= "event-block"
-                  style={{
-                    backgroundColor: getEventColor(detail.covered_a, detail.covered_b),
-                }}
-                >
-                  {detail.event}
-                </div>
+                <TooltipProvider>
+                <Tooltip>
+    <TooltipTrigger asChild>
+        <div
+            key={index}
+            className="event-block"
+            style={{
+                backgroundColor: getEventColor(detail.covered_a, detail.covered_b),
+            }}
+        >
+            {detail.event}
+        </div>
+    </TooltipTrigger>
+    <TooltipContent className="bg-white text-black p-2 rounded-md shadow-lg">
+  <strong>Event covered by:</strong>
+  <div className="flex items-center mt-1">
+    <div className="w-2.5 h-2.5 bg-[hsl(var(--chart-2))] mr-1.5"></div>
+    Set A: {detail.covered_a}x
+  </div>
+  <div className="flex items-center mt-1">
+    <div className="w-2.5 h-2.5 bg-[hsl(var(--chart-3))] mr-1.5"></div>
+    Set B: {detail.covered_b}x
+  </div>
+</TooltipContent>
+</Tooltip>
+</TooltipProvider>
                 
               </div>
             ))}
